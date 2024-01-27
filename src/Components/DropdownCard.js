ee21251725c545e2fe22876aac692d7329a713f4
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2024-01-26 22:05:02 -0800

    Update DropdownCard.js

--- src/Components/DropdownCard.js
+++ src/Components/DropdownCard.js
@@ -1,5 +1,5 @@
 /* Purpose: Displays article as a dropdown card which includes the article's title, image, source, and publication date. When clicked on, card toggles
-open, revealing a sumamry and link to the full article. Clidking on the card again will toggle it closed. */
+open, revealing a summary and link to the full article. Clicking on the card again will toggle it closed. */
 
 import React, { useState } from "react";
 import "../Styles/DropdownCard.css";
